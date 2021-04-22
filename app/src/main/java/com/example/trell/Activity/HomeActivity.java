e92e1bc89495e9f50dac13e7c373bce95b6f8dc1
utsavkuchhal <utsavkuchhal2420@gmail.com>
2021-04-22 21:17:19 +0530

    "Home Activity Added"

---
+++ app/src/main/java/com/example/trell/Activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.trell.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.trell.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
