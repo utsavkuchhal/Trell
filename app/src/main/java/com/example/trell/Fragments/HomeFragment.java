b3611c7f713b3a63936987f6e7dec09b648b7b12
utsavkuchhal <utsavkuchhal2420@gmail.com>
2021-04-23 23:49:15 +0530

    "Main Home Fragment Added"

--- app/src/main/java/com/example/trell/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/trell/Fragments/HomeFragment.java
@@ -9,8 +9,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.trell.Activity.CategoryActivity;
 import com.example.trell.Activity.CreatorsProfileActivity;
 import com.example.trell.R;
 
@@ -22,6 +24,7 @@ public class HomeFragment extends Fragment {
 
     CircleImageView iv_profile;
     ImageView imageView;
+    TextView btn_category;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,11 +32,19 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         iv_profile = view.findViewById(R.id.iv_profile);
         imageView = view.findViewById(R.id.image_view);
+        btn_category = view.findViewById(R.id.btn_category);
         Glide.with(getContext()).load(R.drawable.video).into(imageView);
         iv_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getContext(), CreatorsProfileActivity.class));
+            }
+        });
+
+        btn_category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), CategoryActivity.class));
             }
         });
         return view;
---
+++ app/src/main/java/com/example/trell/Fragments/MainHomeFragment.java
@@ -0,0 +1,28 @@
+package com.example.trell.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.trell.R;
+
+public class MainHomeFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_home, container, false);
+        return view;
+    }
+}
