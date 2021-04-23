7d571d5d7e026dbdbfc58ae5c0e2a99d1ddafccc
utsavkuchhal <utsavkuchhal2420@gmail.com>
2021-04-23 13:27:50 +0530

    "Pie  Chart Added"

--- app/src/main/java/com/example/trell/Fragments/StatsFragment.java
+++ app/src/main/java/com/example/trell/Fragments/StatsFragment.java
@@ -1,5 +1,6 @@
 package com.example.trell.Fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,21 +11,22 @@ import android.view.ViewGroup;
 
 import com.example.trell.R;
 
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
 
 public class StatsFragment extends Fragment {
-
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stats, container, false);
+        View view =  inflater.inflate(R.layout.fragment_stats, container, false);
+        PieChart mPieChart = (PieChart) view.findViewById(R.id.piechart);
+        mPieChart.addPieSlice(new PieModel("Subscribed Users", 15, Color.parseColor("#FE6DA8")));
+        mPieChart.addPieSlice(new PieModel("Active Users", 25, Color.parseColor("#56B7F1")));
+        mPieChart.addPieSlice(new PieModel("Users Completed", 35, Color.parseColor("#CDA67F")));
+        mPieChart.startAnimation();
+        return  view;
     }
 }
